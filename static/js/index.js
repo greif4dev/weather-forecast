4457040627f00cbf741f06787b950c43ea3dbf54
greif4dev <joaopedrogreif@gmail.com>
2023-12-06 23:30:42 -0300

    perf(menu) nova implementacao do menu na aplicacao

--- static/js/index.js
+++ static/js/index.js
@@ -10,3 +10,16 @@ function toggleMenu() {
       menuIcon.style.display = "none";
     }
   }
+
+const menu = 
+  ` 
+    <div class="close-btn" onclick="toggleMenu()">×</div>
+    <a href="#"><i class="fa-solid fa-user" style="color: #ffffff;"></i>ㅤPerfil</a>
+    <a href="#"><i class="fa-solid fa-clock-rotate-left"></i>ㅤHistórico</a>
+    <a href="/mapa"><i class="fa-solid fa-map-location-dot" style="color: #ffffff;"></i>ㅤMapa</a>
+    <div class="logout">
+      <a href="#"><i class="fa-solid fa-right-from-bracket" style="color: #ffffff;"></i>ㅤLogout</a>
+    </div>
+  <div id="menu-icon" onclick="toggleMenu()">☰</div>`;
+
+document.getElementById('menu').innerHTML = menu;
